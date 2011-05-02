7d17e9e00a00ee70e9aa920541955a0d819a4fd4
Paul Sadauskas <psadauskas@gmail.com>
2011-05-02 17:35:14 -0600

    Don't link to metric_type for now, since its embedded in metric

--- lib/ssbe/models/core/metric.rb
+++ lib/ssbe/models/core/metric.rb
@@ -7,13 +7,9 @@ class Metric
   persists :path, :metric_type, :active, :status, :value, :historical_observations_href, :active?
 
   links_to :host
-  links_to :metric_type
+  #links_to :metric_type  # NOTE: collides with {metric_type} hash embedded in metric document
   links_to_collection :observations
   links_to_collection :matching_filters, :class_name => "MetricFilter"
-
-  def name
-    metric_type["path"]
-  end
 
   def self.make(*args)
     attrs = plan(*args)
