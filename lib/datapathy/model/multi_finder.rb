d6789b22ccc7f45b2ad919f692fe637471aacf6d
Adam Vaughan <ajv@absolute-performance.com>
2011-10-19 08:48:17 -0600

    change in method to accept multiple argument types

--- lib/datapathy/model/multi_finder.rb
+++ lib/datapathy/model/multi_finder.rb
@@ -7,14 +7,48 @@ module Datapathy::Model
     extend ActiveSupport::Concern
 
     module ClassMethods
-      def in(model_hrefs, options = {})
+      # Fetches all models in an array of hrefs. The hrefs can be specified as
+      # either a collection of hrefs or as a collection of objects and a method
+      # to map the objects to.
+      #
+      # Examples:
+      #
+      # You can pass an array of hrefs:
+      #   Model.in(['http://example.com/test/1', 'http://example.com/test/2'])
+      #
+      # Or you can just pass a series of hrefs:
+      #   Model.in('http://example.com/test/1', 'http://example.com/test/2')
+      #
+      # Or a collection of parent objects and the method to map them to to get
+      # the collection of hrefs:
+      #   Model.in(parent_models, :children_hrefs)
+      #
+      # This is equivalent to calling
+      #   Model.in(parent_models.map(&:children_hrefs).flatten)
+      #
+      # A hash is returned where the key is the href and the value is the model.
+      #
+      def in(*args)
+        case args.length
+        when 1
+          model_hrefs = [args.first].flatten
+        when 2
+          if args.first.kind_of? String
+            model_hrefs = args
+          else
+            model_hrefs = args.first.map { |model| model.send(args.second) }
+          end
+        else
+          model_hrefs = args
+        end
+
         collection = Datapathy::Collection.new(self)
-        href = options[:href] || collection.href
+        href = collection.href
         regex = /^#{href}\//
 
-        ids = model_hrefs.map do |model_href|
+        ids = model_hrefs.flatten.uniq.map do |model_href|
           if model_href =~ regex
-            model_href.gsub(regex, '') if model_href =~ regex
+            model_href.gsub(regex, '')
           else
             model_href.split(/\//).last
           end
@@ -22,7 +56,7 @@ module Datapathy::Model
 
         collection.href = Addressable::URI.parse(href)
         collection.href.query_values = {:ids => ids}
-        collection
+        collection.inject({}) { |hash, model| hash.merge(model.href => model) }
       end
     end
   end
