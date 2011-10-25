3b05de6b2a39f4cef8578959737ec625d294db33
joanne cheng <chengjoanne2@gmail.com>
2011-10-25 14:50:09 -0600

    Added Observation.in method for multiple observations

--- lib/ssbe/models/core/observation.rb
+++ lib/ssbe/models/core/observation.rb
@@ -1,6 +1,5 @@
 class Observation
   include Datapathy::Model
-  include Datapathy::Model::MultiFinder
 
   service_type :measurements
 
@@ -9,4 +8,19 @@ class Observation
   links_to :metric
   links_to :host
   links_to :agent
+
+  def self.in(hrefs, start_interval, end_interval)
+    regex = /\D+(\d+)/
+    ids = hrefs.map do |model_href|
+      if model_href =~ regex
+        regex.match(model_href)[1]
+      else
+        model_href.split(/\//).last
+      end
+    end.compact.join(',')
+    uri = "#{ServiceDescriptor.discover(:measurements, "MultipleObservations")}?metric_ids=#{ids}&start=#{start_interval}&end=#{end_interval}"
+    response = Datapathy.adapters[:ssbe].http.resource(uri).get(:accept => 'application/vnd.absperf.ssmj1+json')
+
+    JSON.parse(response.body).map { |observation| observation.with_indifferent_access }
+  end
 end
